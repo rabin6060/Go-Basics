bfdba1a285ce29e54782e3805266266b66c0c007
Rabin Mainali <rabinmainali1999@gmail.com>
2025-07-16 16:03:24 +0545

    added chi middlewares and define route paths

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,6 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"time"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 
 type application struct {
@@ -11,13 +17,36 @@ type config struct{
 	addr string
 }
 
-func (app *application) run() error {
-	mux := http.NewServeMux()
+func (app *application) mount() http.Handler {
+		r := chi.NewRouter()
+
+		//middlewares
+		r.Use(middleware.RequestID)
+  		r.Use(middleware.RealIP)
+		r.Use(middleware.Logger)
+		r.Use(middleware.Recoverer) //recovers from panics 
+
+		r.Use(middleware.Timeout(60 * time.Second))
+
+		r.Route("/v1",func(r chi.Router) {
+			r.Get("/health",app.healthCheckHandler)
+		})
+		
+
+		return r
+}
+
+func (app *application) run(mux http.Handler) error {
+	
 
 	srv := http.Server{
 		Addr: app.config.addr,
 		Handler: mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout: time.Second * 10,
+		IdleTimeout: time.Minute,
+		
 	}
-
+	log.Printf("server is started at %s",app.config.addr)
 	return srv.ListenAndServe()
 }
