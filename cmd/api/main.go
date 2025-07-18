6a0859a2b55b9578ee8dd1de32a90b415de03984
Rabin Mainali <rabinmainali1999@gmail.com>
2025-07-18 22:09:31 +0545

    db connection completed

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,20 +2,35 @@ package main
 
 import (
 	"log"
-
+	"github.com/rabin6060/Go-Basics/internal/db"
 	"github.com/rabin6060/Go-Basics/internal/env"
 	"github.com/rabin6060/Go-Basics/internal/store"
+
 )
 
 func main(){
-
-	store := store.NewStorage(nil) 
+	cfg := config{
+			addr: env.GetString("Addr","=:8001"),
+			db:dbConfig{
+				addr: env.GetString("Addr","postgres://postgres:postgres@localhost/social?sslmode=disable"),
+				maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS",30),
+				maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS",30),
+				maxIdleTime: env.GetString("DB_MAX_IDLE_TIME","15m"),
+			},
+	}
+	 
+	db,err:=db.New(cfg.addr,cfg.db.maxOpenConns,cfg.db.maxIdleConns,cfg.db.maxIdleTime)
+	if err!=nil{
+		log.Panic(err)
+	}
+	defer db.Close()
+	log.Println("database connected")
+	store := store.NewStorage(db)
 	app := &application{
-		config: config{
-			addr: env.GetString("Addr",":8001"),
-		},
+		config: cfg,
 		store: store,
 	}
+
 	mux := app.mount()
 	log.Fatal(app.run(mux))
 
