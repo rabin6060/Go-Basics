a6017af82f9cb6755feeb8cd4ddc688d817f6eec
Rabin Mainali <rabinmainali1999@gmail.com>
2025-07-18 22:12:11 +0545

    db connection completed

---
+++ internal/db/db.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"time"
+	_ "github.com/lib/pq"
+)
+
+
+func New(addr string,maxOpenConns, maxIdleConns int,maxIdleTime string)(*sql.DB,error){
+	db,err:=sql.Open("postgres",addr)
+	if err!=nil {
+		return  nil,err
+	}
+	duration,err := time.ParseDuration(maxIdleTime)
+	if err!= nil {
+		return nil,err
+	}
+	db.SetMaxOpenConns(maxOpenConns)
+	db.SetMaxIdleConns(maxIdleConns)
+	db.SetConnMaxIdleTime(duration)
+	ctx,cancel := context.WithTimeout(context.Background(),5*time.Second)
+	defer cancel()
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil,err
+	}
+	return db,nil
+}
